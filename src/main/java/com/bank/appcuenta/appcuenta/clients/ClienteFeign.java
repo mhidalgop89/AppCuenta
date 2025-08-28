af12c7e36f77e9133ac88b96ac0faeede3d54783
mhidalgo <mario.hidalgo89@hotmail.com>
2025-08-27 21:39:45 -0500

    feature/docker scope

--- src/main/java/com/bank/appcuenta/appcuenta/clients/ClienteFeign.java
+++ src/main/java/com/bank/appcuenta/appcuenta/clients/ClienteFeign.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "AppClient", url = "localhost:8080/clientes")
+@FeignClient(name = "AppClient", url = "clients-svc:8080/clientes")
 public interface ClienteFeign {
 
     @GetMapping("/{identificacion}")
